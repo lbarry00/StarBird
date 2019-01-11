86dd38512fcbbf42ef2a81550964f2b14a6982c5
abrandsness <abrandsness21@my.whitworth.edu>
2019-01-11 15:28:01 -0800

    Bird stays in screen, kinda

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,13 +1,17 @@
 
  $( "body" ).keydown(function(event) { //triggers function on keydown
     if ( event.which == 40 ) { //down button moves bird immage down 50 px
+        if($(".bird").position().top < 550) { //if the bird goes below 550 px the animation won't activate
         $( ".bird" ).animate({
         top: "+=50" });
+        }
     }
     else if (event.which == 38 ) { //up button moves up 50 px
-        $( ".bird" ).animate({
-        top: "-=50" });
+        if($(".bird").position().top > 150) {//above 150 px the animation won't activate
+            $( ".bird" ).animate({
+        top: "-=25" });
     }    
+    }
 });
 
 
