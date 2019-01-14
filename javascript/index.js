225291811ccb14540de4daff36bc39c048b12816
AlmatYeraly <ayeraly20@my.whitworth.edu>
2019-01-13 18:11:35 -0800

    coded js so that the bird stays within the boundaries of the background image

--- javascript/index.js
+++ javascript/index.js
@@ -1,31 +1,40 @@
-function initialize() {
-    var bird = document.getElementById("bird");
-    bird.style.top = "300px"; 
-    bird.style.left = "50px"; 
-}
-
-initialize(); 
-
-function upArrow(){
-    var bird = document.getElementById("bird");
-    bird.style.top = parseInt(bird.style.top) - 10 + 'px';
-    
-}
-function downArrow(){
-    var bird = document.getElementById("bird");
-    bird.style.top = parseInt(bird.style.top) + 10 + 'px';
-}
-function move(event){
-      switch(event.keyCode){
-        case 38:
-           upArrow();
-        break;
-        case 40:
-            downArrow();
-        break;
-    }
-}
-
-function docReady(){
-    window.addEventListener('keydown', move);
-}
+$(document).ready(function () {
+    $div_name = $("#bird"); //Name of the div you wish to control
+    $speed = 5; //Higher is slower
+    $move = "10"; //Amount you want to move the element by
+    $(document).keydown(function (e) {
+        e.preventDefault();
+        switch (e.which) { 
+            case 38:
+                //up
+                if($div_name.position().top > 150)
+                {
+                    $($div_name).animate({
+                        top: "-=" + $move,
+                    }, $speed, function () {});
+                }
+                else if ($div_name.position().top < 150)
+                {
+                    $($div_name).animate({
+                        top: "-=" + 0,
+                    }, $speed, function () {});
+                }
+                break;
+            case 40:
+                //down
+                if($div_name.position().top < 615)
+                {
+                    $($div_name).animate({
+                        top: "+=" + $move
+                    }, $speed, function () {});
+                }
+                else if($div_name.position().top > 615)
+                {
+                    $($div_name).animate({
+                        top: "+=" + 0
+                    }, $speed, function () {});
+                }
+                break;
+        }
+    });
+});
--- javascript/testindex.js
+++ javascript/testindex.js
@@ -1,21 +1,39 @@
 $(document).ready(function () {
     $div_name = $("#bird"); //Name of the div you wish to control
-    $speed = 100; //Higher is slower
-    $move = "50"; //Amount you want to move the element by
+    $speed = 5; //Higher is slower
+    $move = "10"; //Amount you want to move the element by
     $(document).keydown(function (e) {
         e.preventDefault();
         switch (e.which) { 
             case 38:
                 //up
-                $($div_name).animate({
-                    top: "-=" + $move,
-                }, $speed, function () {});
+                if($div_name.position().top > 150)
+                {
+                    $($div_name).animate({
+                        top: "-=" + $move,
+                    }, $speed, function () {});
+                }
+                else if ($div_name.position().top < 150)
+                {
+                    $($div_name).animate({
+                        top: "-=" + 0,
+                    }, $speed, function () {});
+                }
                 break;
             case 40:
                 //down
-                $($div_name).animate({
-                    top: "+=" + $move
-                }, $speed, function () {});
+                if($div_name.position().top < 615)
+                {
+                    $($div_name).animate({
+                        top: "+=" + $move
+                    }, $speed, function () {});
+                }
+                else if($div_name.position().top > 615)
+                {
+                    $($div_name).animate({
+                        top: "+=" + 0
+                    }, $speed, function () {});
+                }
                 break;
         }
     });
