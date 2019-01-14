a8f06818c12f0c3912a4c9e11fc43e724a628736
Tyler J Seymour <tseymour18@my.whitworth.edu>
2019-01-14 10:26:40 -0800

    Made positioning dynamic and improved JS

--- js/bird.js
+++ js/bird.js
@@ -1,28 +1,35 @@
 var $bird;
+var $gameContainer;
 var height;
-
+ /* Get variables ready and make a bird. */
 $(document).ready(function() {
-    $bird = $("<img class='bird' id='bird' src='img/horizontalBird.gif'>")
-    $("#gameContainer").append($bird);
+    $bird = $("<img class='bird' id='bird' src='img/horizontalBird.gif'>");
+    $gameContainer = $('#gameContainer');
+    $gameContainer.append($bird);
     height = parseInt($bird.css('top'), 10);  // 50vh Use $bird.css(top) instead to get height dynamically
 });
 
-function moveUp() {
-    if (height - 2 < 171) {
-        return 1;
+
+/* Control bird movement relative to gameContainer */
+
+function moveUp() { //Uses increments of 20 px, can change without consequences for faster bird move.
+    if (height - 20 < $gameContainer.offset().top){  //check if bird is at top of screen.
+        height = $gameContainer.offset().top;        //If so, set height = top of game container.
     } else {
-        height -= 10;
-        $bird.css('top', height + "px");
+        height -= 20;                                //Else move bird up.
     }
+
+    $bird.css('top', height + "px");
 }
 
 function moveDown() {
-    if (height + 2 > 641) {
-        return 1;
+    if (height + 20 > $gameContainer.offset().top + $gameContainer.outerHeight() - parseInt($gameContainer.css('border-bottom-width')) - parseInt($bird.css('height'))) { //Need to add bird dimensions here to check if bird's bottom is at bottom. Include border check. 
+        height = $gameContainer.offset().top + $gameContainer.outerHeight() - parseInt($gameContainer.css('border-bottom-width')) - parseInt($bird.css('height'));        //Set bird bottom to bottom of gameContainer.
     } else {
-        height += 10;
-        $bird.css('top', height + "px");
+        height += 20; //Else move bird down.
     }
+
+    $bird.css('top', height + "px"); // Actually set bottom here.
 }
 
 $(document).keydown(function(e) {
